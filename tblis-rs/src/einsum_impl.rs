b1fd0b7c5ec8ebf08ca9a41b1e8950374cc33f4f
ajz34 <ajz34@outlook.com>
2025-09-22 14:18:50 +0800

    einsum test

--- tblis-rs/src/einsum_impl.rs
+++ tblis-rs/src/einsum_impl.rs
@@ -5,6 +5,7 @@
 use crate::prelude::*;
 use opt_einsum_path::typing::{ContractionType, SizeLimitType, TensorShapeType};
 use opt_einsum_path::{PathOptimizer, contract_path};
+use std::collections::BTreeSet;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct TblisContractStep {
@@ -12,7 +13,7 @@ pub struct TblisContractStep {
     pub idx_a: String,
     pub idx_b: Option<String>,
     pub idx_c: String,
-    pub shape_c: Vec<usize>,
+    pub shape_c: Vec<isize>,
 }
 
 pub fn tblis_einsum_prep(
@@ -43,14 +44,14 @@ pub fn tblis_einsum_prep_f(
                 let idx_ab_split = einsum_str_split[0].split(',').collect::<Vec<&str>>();
                 let idx_a = idx_ab_split[0].to_string();
                 let idx_b = idx_ab_split[1].to_string();
-                let shape_c = idx_c.chars().map(|c| size_dict[&c]).collect::<Vec<usize>>();
+                let shape_c = idx_c.chars().map(|c| size_dict[&c] as isize).collect();
                 steps.push(TblisContractStep { indices, idx_a, idx_b: Some(idx_b), idx_c, shape_c });
             },
             1 => {
                 let einsum_str_split: Vec<&str> = einsum_str.split("->").collect();
                 let idx_a = einsum_str_split[0].to_string();
                 let idx_c = einsum_str_split[1].to_string();
-                let shape_c = idx_c.chars().map(|c| size_dict[&c]).collect::<Vec<usize>>();
+                let shape_c = idx_c.chars().map(|c| size_dict[&c] as isize).collect();
                 steps.push(TblisContractStep { indices, idx_a, idx_b: None, idx_c, shape_c });
             },
             _ => return Err("Only pairwise / single tensor contractions are supported.".to_string()),
@@ -60,18 +61,21 @@ pub fn tblis_einsum_prep_f(
 }
 
 /// Generate strides by shape and row-major/col-major flag.
-fn shape_to_stride(shape: &[usize], row_major: bool) -> Vec<isize> {
+fn shape_to_stride(shape: &[isize], row_major: bool) -> Vec<isize> {
     let ndim = shape.len();
-    let mut stride = vec![0isize; ndim];
+    let mut stride = vec![1isize; ndim];
+    if ndim == 0 {
+        return stride;
+    }
     if row_major {
         stride[ndim - 1] = 1;
         for i in (0..ndim - 1).rev() {
-            stride[i] = stride[i + 1] * shape[i + 1] as isize;
+            stride[i] = stride[i + 1] * shape[i + 1];
         }
     } else {
         stride[0] = 1;
         for i in 1..ndim {
-            stride[i] = stride[i - 1] * shape[i - 1] as isize;
+            stride[i] = stride[i - 1] * shape[i - 1];
         }
     }
     stride
@@ -90,6 +94,34 @@ where
     tblis_einsum_f(subscripts, operands, optimize, memory_limit, row_major).unwrap()
 }
 
+fn tblis_trace_f<T>(
+    subscript_prev: &str,
+    subscript_traced: &str,
+    tsr_prev: &TblisTensor<T>,
+    row_major: bool,
+) -> Result<(Vec<T>, TblisTensor<T>), String>
+where
+    T: TblisFloatAPI,
+{
+    let idx_prev: Vec<char> = subscript_prev.chars().collect();
+    let idx_traced: Vec<char> = subscript_traced.chars().collect();
+    let subscript_traced = idx_traced.iter().collect::<String>();
+    assert!(idx_prev.len() == tsr_prev.shape.len());
+    assert!(idx_traced.len() <= idx_prev.len());
+    // build shape/stride for traced tensor
+    let shape_traced = idx_traced
+        .iter()
+        .map(|&c| tsr_prev.shape[idx_prev.iter().position(|&x| x == c).unwrap()])
+        .collect::<Vec<isize>>();
+    let stride_traced = shape_to_stride(&shape_traced, row_major);
+    let size_traced = shape_traced.iter().product::<isize>() as usize;
+    let vec_traced = unsafe { crate::alloc_vec::uninitialized_vec::<T>(size_traced)? };
+    let mut tsr_traced = TblisTensor::new(vec_traced.as_ptr() as *mut T, &shape_traced, &stride_traced);
+    let cfg = TblisAddCfgBuilder::default().beta(T::zero()).build().unwrap();
+    crate::tensor_ops::tblis_tensor_add(tsr_prev, subscript_prev, &mut tsr_traced, &subscript_traced, Some(cfg));
+    Ok((vec_traced, tsr_traced))
+}
+
 pub fn tblis_einsum_f<T>(
     subscripts: &str,
     operands: &[&TblisTensor<T>],
@@ -105,23 +137,69 @@ where
     let mut tensor_list: Vec<(TblisTensor<T>, Option<Vec<T>>)> = operands.iter().map(|&t| (t.clone(), None)).collect();
     for step in steps.iter() {
         let TblisContractStep { indices, idx_a, idx_b, idx_c, shape_c } = step;
+
         if let Some(idx_b) = idx_b {
             // case of tensor mult
+            let mut idx_a = idx_a.clone();
+            let mut idx_b = idx_b.clone();
+
+            // tblis_tensor_mult is not able to handle combined contract with trace
+            // handle trace here
+            let idx_a_set = idx_a.chars().collect::<BTreeSet<char>>();
+            let idx_b_set = idx_b.chars().collect::<BTreeSet<char>>();
+            let idx_c_set = idx_c.chars().collect::<BTreeSet<char>>();
+            let idx_a_only = &(&idx_a_set - &idx_b_set) - &idx_c_set;
+            let idx_b_only = &(&idx_b_set - &idx_a_set) - &idx_c_set;
+            if !idx_a_only.is_empty() {
+                let tsr_a = &tensor_list[indices[0]].0;
+                let idx_a_traced = idx_a.chars().filter(|c| !idx_a_only.contains(c)).collect::<String>();
+                let (vec_a_traced, tsr_a_traced) = tblis_trace_f(&idx_a, &idx_a_traced, tsr_a, row_major)?;
+                tensor_list[indices[0]] = (tsr_a_traced, Some(vec_a_traced));
+                idx_a = idx_a_traced;
+            }
+            if !idx_b_only.is_empty() {
+                let tsr_b = &tensor_list[indices[1]].0;
+                let idx_b_traced = idx_b.chars().filter(|c| !idx_b_only.contains(c)).collect::<String>();
+                let (vec_b_traced, tsr_b_traced) = tblis_trace_f(&idx_b, &idx_b_traced, tsr_b, row_major)?;
+                tensor_list[indices[1]] = (tsr_b_traced, Some(vec_b_traced));
+                idx_b = idx_b_traced;
+            }
+
             let tsr_a = &tensor_list[indices[0]].0;
             let tsr_b = &tensor_list[indices[1]].0;
-            let vec_c = unsafe { crate::alloc_vec::uninitialized_vec::<T>(shape_c.iter().product())? };
+            let size_c = shape_c.iter().product::<isize>() as usize;
+            let vec_c = unsafe { crate::alloc_vec::uninitialized_vec::<T>(size_c)? };
             let stride_c = shape_to_stride(shape_c, row_major);
-            let shape_c = shape_c.iter().map(|&s| s as isize).collect::<Vec<isize>>();
-            let mut tsr_c = TblisTensor::new(vec_c.as_ptr() as *mut T, shape_c, stride_c);
-            crate::tensor_ops::tblis_tensor_mult(tsr_a, idx_a, tsr_b, idx_b, &mut tsr_c, idx_c, None);
+            let mut tsr_c = TblisTensor::new(vec_c.as_ptr() as *mut T, shape_c, &stride_c);
+            // handle empty idx_a/idx_b (scalar-like operations)
+            match (idx_a.is_empty(), idx_b.is_empty()) {
+                (false, false) => {
+                    crate::tensor_ops::tblis_tensor_mult(tsr_a, &idx_a, tsr_b, &idx_b, &mut tsr_c, idx_c, None);
+                },
+                (true, true) => {
+                    let val_a = tsr_a.clone().set_scalar(T::one()).to_scalar()?;
+                    let val_b = tsr_b.clone().set_scalar(T::one()).to_scalar()?;
+                    unsafe { *tsr_c.data = val_a * val_b };
+                },
+                (true, false) => {
+                    let val_a = tsr_a.clone().set_scalar(T::one()).to_scalar()?;
+                    let add_cfg = TblisAddCfgBuilder::default().alpha(val_a).beta(T::zero()).build().unwrap();
+                    crate::tensor_ops::tblis_tensor_add(tsr_b, &idx_b, &mut tsr_c, idx_c, Some(add_cfg));
+                },
+                (false, true) => {
+                    let val_b = tsr_b.clone().set_scalar(T::one()).to_scalar()?;
+                    let add_cfg = TblisAddCfgBuilder::default().alpha(val_b).beta(T::zero()).build().unwrap();
+                    crate::tensor_ops::tblis_tensor_add(tsr_a, &idx_a, &mut tsr_c, idx_c, Some(add_cfg));
+                },
+            };
             tensor_list.push((tsr_c, Some(vec_c)));
         } else {
             // case of tensor transpose (implement by add)
             let tsr_a = &tensor_list[indices[0]].0;
-            let vec_c = unsafe { crate::alloc_vec::uninitialized_vec::<T>(shape_c.iter().product())? };
+            let size_c = shape_c.iter().product::<isize>() as usize;
+            let vec_c = unsafe { crate::alloc_vec::uninitialized_vec::<T>(size_c)? };
             let stride_c = shape_to_stride(shape_c, row_major);
-            let shape_c = shape_c.iter().map(|&s| s as isize).collect::<Vec<isize>>();
-            let mut tsr_c = TblisTensor::new(vec_c.as_ptr() as *mut T, shape_c, stride_c);
+            let mut tsr_c = TblisTensor::new(vec_c.as_ptr() as *mut T, shape_c, &stride_c);
             let cfg = TblisAddCfgBuilder::default().beta(T::zero()).build().unwrap();
             crate::tensor_ops::tblis_tensor_add(tsr_a, idx_a, &mut tsr_c, idx_c, Some(cfg));
             tensor_list.push((tsr_c, Some(vec_c)));
@@ -151,10 +229,10 @@ pub mod ndarray_einsum {
         D: Dimension,
     {
         unsafe fn to_tblis_tensor(&self) -> TblisTensor<T> {
-            let shape = self.shape().iter().map(|&s| s as isize).collect();
+            let shape = self.shape().iter().map(|&s| s as isize).collect::<Vec<isize>>();
             let stride = self.strides().to_vec();
             let data_ptr = self.as_ptr() as *mut T;
-            TblisTensor::new(data_ptr, shape, stride)
+            TblisTensor::new(data_ptr, &shape, &stride)
         }
     }
 
---
+++ tblis-rs/tests/test_f64.rs
@@ -0,0 +1,131 @@
+#![allow(clippy::excessive_precision)]
+
+use rstest::rstest;
+use tblis_rs::prelude::*;
+
+fn gen_array(shape: &[isize]) -> Vec<f64> {
+    let size = shape.iter().product();
+    (0..size).map(|i| (i as f64 + 0.2).cos()).collect()
+}
+
+fn fp(vec: &[f64]) -> f64 {
+    vec.iter().enumerate().map(|(i, &x)| (i as f64).cos() * x).sum()
+}
+
+fn build_shape(s: &str) -> Vec<isize> {
+    let valid_chars = "abcdefghijklmnopqABC";
+    let sizes = [2, 3, 4, 5, 4, 3, 2, 6, 5, 4, 3, 2, 5, 7, 4, 3, 2, 3, 4, 9, 10, 2, 4, 5, 3, 2, 6];
+    s.chars().map(|c| valid_chars.find(c).map(|i| sizes[i]).unwrap_or(1)).collect()
+}
+
+fn build_strides(shape: &[isize]) -> Vec<isize> {
+    // build row-major strides
+    let mut strides = vec![1isize; shape.len()];
+    if shape.is_empty() {
+        return strides;
+    }
+    for i in (0..shape.len() - 1).rev() {
+        strides[i] = strides[i + 1] * shape[i + 1];
+    }
+    strides
+}
+
+fn build_tblis_tensor(s: &str) -> (Vec<f64>, TblisTensor<f64>) {
+    let shape = build_shape(s);
+    let strides = build_strides(&shape);
+    let mut data = gen_array(&shape);
+    let tensor = TblisTensor::new(data.as_mut_ptr(), &shape, &strides);
+    (data, tensor)
+}
+
+#[rstest]
+// [TYPE] scalar-like operations
+#[case("a,->a"                                 , vec![2]                 ,   1.1524106074800669)]
+#[case("ab,->ab"                               , vec![2, 3]              ,   2.8053551615173387)]
+#[case(",ab,->ab"                              , vec![2, 3]              ,   2.7494348327775620)]
+#[case(",,->"                                  , vec![]                  ,   0.9413838371083508)]
+// [TYPE] hadamard-like products
+#[case("a,ab,abc->abc"                         , vec![2, 3, 4]           ,   1.1706257404816276)]
+#[case("a,b,ab->ab"                            , vec![2, 3]              ,   1.2343776816309531)]
+// [TYPE] index-transformations
+#[case("ea,fb,gc,hd,abcd->efgh"                , vec![4, 3, 2, 6]        ,  12.1332755429121821)]
+#[case("ea,fb,abcd,gc,hd->efgh"                , vec![4, 3, 2, 6]        ,  12.1332755429121750)]
+#[case("abcd,ea,fb,gc,hd->efgh"                , vec![4, 3, 2, 6]        ,  12.1332755429121910)]
+// [TYPE] complex contractions
+#[case("acdf,jbje,gihb,hfac,gfac,gifabc,hfac"  , vec![5, 4]              ,   0.1814690776365560)]
+#[case("acdf,jbje,gihb,hfac,gfac,gifabc,hfac"  , vec![5, 4]              ,   0.1814690776365560)]
+#[case("cd,bdhe,aidb,hgca,gc,hgibcd,hgac"      , vec![4]                 ,  -4.4698469414635698)]
+#[case("abhe,hidj,jgba,hiab,gab"               , vec![5, 4]              , -16.9026985074718148)]
+#[case("bde,cdh,agdb,hica,ibd,hgicd,hiac"      , vec![4]                 ,  -5.9363077640651882)]
+#[case("chd,bde,agbc,hiad,hgc,hgi,hiad"        , vec![4]                 ,  -0.3797503358113263)]
+#[case("chd,bde,agbc,hiad,bdi,cgh,agdb"        , vec![4]                 ,   9.3347856408443466)]
+#[case("bdhe,acad,hiab,agac,hibd"              , vec![4, 2]              ,   0.0817842308123281)]
+// [TYPE] collapse
+#[case("ab,ab,c->"                             , vec![]                  ,  -0.7073635910335989)]
+#[case("ab,ab,c->c"                            , vec![4]                 ,   6.9733109228592252)]
+#[case("ab,ab,cd,cd->"                         , vec![]                  ,  30.2845404955025153)]
+#[case("ab,ab,cd,cd->ac"                       , vec![2, 4]              ,   5.1155948313305153)]
+#[case("ab,ab,cd,cd->cd"                       , vec![4, 5]              ,   2.9488487899924660)]
+#[case("ab,ab,cd,cd,ef,ef->"                   , vec![]                  , 177.9078409242766838)]
+// [TYPE] outer products
+#[case("ab,cd,ef->abcdef"                      , vec![2, 3, 4, 5, 4, 3]  ,   7.2612571312420187)]
+#[case("ab,cd,ef->acdf"                        , vec![2, 4, 5, 3]        ,   0.2218804695072113)]
+#[case("ab,cd,de->abcde"                       , vec![2, 3, 4, 5, 4]     ,   5.6145252091023128)]
+#[case("ab,cd,de->be"                          , vec![3, 4]              ,  -0.0112868166168827)]
+#[case("ab,bcd,cd->abcd"                       , vec![2, 3, 4, 5]        ,   1.2455894084757055)]
+#[case("ab,bcd,cd->abd"                        , vec![2, 3, 5]           ,   1.1900669479088557)]
+// [TYPE] cases that have previously failed
+#[case("eb,cb,fb->cef"                         , vec![4, 4, 3]           ,  -0.3948422850851117)]
+#[case("dd,fb,be,cdb->cef"                     , vec![4, 4, 3]           ,   0.0433826323804478)]
+#[case("bca,cdb,dbf,afc->"                     , vec![]                  ,   1.8790765302648753)]
+#[case("dcc,fce,ea,dbf->ab"                    , vec![2, 3]              ,  -0.0995815485371659)]
+#[case("fdf,cdd,ccd,afe->ae"                   , vec![2, 4]              ,  -1.8204695389889403)]
+#[case("abcd,ad"                               , vec![3, 4]              ,  -6.2411233103176258)]
+#[case("ed,fcd,ff,bcf->be"                     , vec![3, 4]              ,   0.5335137116293984)]
+#[case("baa,dcf,af,cde->be"                    , vec![3, 4]              ,   6.7338928837379548)]
+#[case("bd,db,eac->ace"                        , vec![2, 4, 4]           ,   0.5294245507683408)]
+#[case("fff,fae,bef,def->abd"                  , vec![2, 3, 5]           ,  -6.1168007036329932)]
+#[case("efc,dbc,acf,fd->abe"                   , vec![2, 3, 4]           ,   2.1288617659874238)]
+// [TYPE] Inner products
+#[case("ab,ab"                                 , vec![]                  ,   2.8946232078487490)]
+#[case("ab,ba"                                 , vec![]                  ,   1.3829944294961767)]
+#[case("abc,abc"                               , vec![]                  ,  12.0867609307166504)]
+#[case("abc,bac"                               , vec![]                  ,   0.7467158730806971)]
+#[case("abc,cba"                               , vec![]                  ,   1.1905629015868808)]
+// [TYPE] GEMM test cases
+#[case("ab,bc"                                 , vec![2, 4]              ,   3.0203306308641209)]
+#[case("ab,cb"                                 , vec![2, 4]              ,   2.4279291273425985)]
+#[case("ba,bc"                                 , vec![2, 4]              ,   0.6630550300956001)]
+#[case("ba,cb"                                 , vec![2, 4]              ,   1.0775626157664493)]
+#[case("abcd,cd"                               , vec![2, 3]              ,  28.5705554819948730)]
+#[case("abcd,ab"                               , vec![4, 5]              ,  29.2315920761960335)]
+#[case("abcd,cdef"                             , vec![2, 3, 4, 3]        , -13.9084614811298160)]
+#[case("abcd,cdef->feba"                       , vec![3, 4, 3, 2]        ,   0.8964372441283897)]
+#[case("abcd,efdc"                             , vec![2, 3, 4, 3]        ,   6.2879216163017242)]
+// [TYPE] Inner than dot
+#[case("aab,bc->ac"                            , vec![2, 4]              ,   2.7142044202227504)]
+#[case("ab,bcc->ac"                            , vec![2, 4]              ,   0.6687831883550068)]
+#[case("aab,bcc->ac"                           , vec![2, 4]              ,   0.5820900839998362)]
+#[case("baa,bcc->ac"                           , vec![2, 4]              ,   5.3442670128010361)]
+#[case("aab,ccb->ac"                           , vec![2, 4]              ,   0.3602374930760941)]
+// [TYPE] Randomly build test caes
+#[case("aab,fa,df,ecc->bde"                    , vec![3, 5, 4]           ,  -0.5103260493034085)]
+#[case("ecb,fef,bad,ed->ac"                    , vec![2, 4]              ,  -1.1285608137144998)]
+#[case("bcf,bbb,fbf,fc->"                      , vec![]                  ,   0.5607438675587967)]
+#[case("bb,ff,be->e"                           , vec![4]                 ,   0.4876389976600121)]
+#[case("bcb,bb,fc,fff->"                       , vec![]                  ,   0.1186616195257864)]
+#[case("fbb,dfd,fc,fc->"                       , vec![]                  ,  -0.1797709337347626)]
+#[case("afd,ba,cc,dc->bf"                      , vec![3, 3]              ,  -0.8876593529208174)]
+#[case("adb,bc,fa,cfc->d"                      , vec![5]                 ,   0.7860343998485582)]
+#[case("bbd,bda,fc,db->acf"                    , vec![2, 4, 3]           ,   2.2992302890561374)]
+#[case("dba,ead,cad->bce"                      , vec![3, 4, 4]           ,   0.7414744179866388)]
+#[case("aef,fbc,dca->bde"                      , vec![3, 5, 4]           ,   1.4158672478067489)]
+fn playground(#[case] einsum_str: &str, #[case] ref_shape: Vec<isize>, #[case] ref_fp: f64) {
+    let einsum_str_inp = einsum_str.split("->").next().unwrap().split(',').collect::<Vec<&str>>();
+    let tensors = einsum_str_inp.iter().map(|s| build_tblis_tensor(s)).collect::<Vec<_>>();
+    let tblis_tensors = tensors.iter().map(|(_, t)| t).collect::<Vec<_>>();
+    let (out_data, out_tensor) = tblis_einsum(einsum_str, &tblis_tensors, true, None, true);
+    let out_fp = fp(&out_data);
+    assert_eq!(out_tensor.shape, ref_shape);
+    assert!((out_fp - ref_fp).abs() < 1e-10);
+}
